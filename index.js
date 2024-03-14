87b96cf4eb2ebb7a5612501fef9e74c5c95f4032
Ilan Servais <145970490+ilan-servais@users.noreply.github.com>
2024-03-14 21:53:24 +0100

    add audio
    ajout de lecture auto audio d'un fichier mp3

--- index.js
+++ index.js
@@ -298,3 +298,25 @@ $(window).on("load", function() {
     });
  });
  
+// Fonction pour changer le volume
+function changeVolume() {
+    var player = document.getElementById('musicPlayer');
+    var volume = document.getElementById('volumeControl').value;
+    player.volume = volume;
+}
+
+// Fonction pour mettre en pause ou jouer la musique
+function togglePlayPause() {
+    var player = document.getElementById('musicPlayer');
+    if (player.paused) {
+        player.play();
+    } else {
+        player.pause();
+    }
+}
+
+// Définir le volume initial à 5% au chargement de la page
+window.onload = function() {
+    var player = document.getElementById('musicPlayer');
+    player.volume = 0.05;
+};
